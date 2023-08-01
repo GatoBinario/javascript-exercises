44253fd1e90b642c3ab7ecaa007bf24f12c5f2c2
GatoBinario <43298345+GatoBinario@users.noreply.github.com>
2023-08-02 00:08:23 +0200

    solve 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, times) {
+    if(times < 0) {
+        return "ERROR";
+    }
+    let returnedText = "";
+    for (let i = 0; i < times; i++) {
+        returnedText += string;
+    }
+    return returnedText;
 };
 
 // Do not edit below this line
