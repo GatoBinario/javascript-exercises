44b3396fa5261adb431dabe3236be01b8833c060
GatoBinario <43298345+GatoBinario@users.noreply.github.com>
2023-08-06 16:55:34 +0200

    Solve 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,33 +1,45 @@
-const add = function() {
-	
+const add = function(a, b) {
+    return a+b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a-b;
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+    for(let i = 0; i<array.length; i++) {
+        sum+=array[i]
+    }
+    return sum;
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+	let sum = 1;
+    for(let i = 0; i<array.length; i++) {
+        sum*=array[i]
+    }
+    return sum;
 };
 
-const power = function() {
-	
+const power = function(a, n) {
+    return Math.pow(a, n);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result*=i
+    }
+    return result;
 };
 
 // Do not edit below this line
 module.exports = {
-  add,
-  subtract,
-  sum,
-  multiply,
-  power,
-  factorial
+    add,
+    subtract,
+    sum,
+    multiply,
+    power,
+    factorial
 };
