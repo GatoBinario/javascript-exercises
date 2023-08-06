bdd0478e646e3146faa0e93a81095dc43f7a2fec
GatoBinario <43298345+GatoBinario@users.noreply.github.com>
2023-08-06 15:51:40 +0200

    Solve 5

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,25 @@
-const sumAll = function() {
-
+const sumAll = function(startN, endN) {
+    if(typeof startN !== "number" || typeof endN !== "number") {
+        return "ERROR";
+    }
+    else if(startN < 0 || endN < 0) {
+        return "ERROR";
+    }
+    let sum = 0;
+    if(startN <= endN) {
+        for(let i = startN; i<=endN; i++) {
+            sum+=i;
+        }
+    }
+    else {
+        for(let i = endN; i<=startN; i++) {
+            sum+=i;
+        }
+    }
+    return sum;
 };
+
+let x = sumAll(1,3);
 
 // Do not edit below this line
 module.exports = sumAll;
