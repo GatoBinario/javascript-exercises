7bb7258dd201d766cb5b3f07fde4878508e62277
GatoBinario <43298345+GatoBinario@users.noreply.github.com>
2023-08-06 16:41:07 +0200

    Solve 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,11 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fDegrees) {
+    return Math.round(5/9*(fDegrees-32)*10)/10
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(cDegrees) {
+    return Math.round((9/5*cDegrees+32)*10)/10
 };
 
 // Do not edit below this line
 module.exports = {
-  convertToCelsius,
-  convertToFahrenheit
+    convertToCelsius,
+    convertToFahrenheit
 };
