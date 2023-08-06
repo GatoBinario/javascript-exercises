adae1669c0954dc2ac0d63576210b6a7fd479fc6
GatoBinario <43298345+GatoBinario@users.noreply.github.com>
2023-08-06 15:34:18 +0200

    Solve 4

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,23 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let newArray = array;
+    for(let i = 0; i<args.length; i++) {
+        newArray = removeElementFromArray(array, args[i])
+    }
+    return newArray;
 };
+
+function removeElementFromArray(array, element) {
+    const index = array.indexOf(element);
+    if (index !== -1) {
+        const x = array.splice(index, 1);
+    }
+    return array;
+}
+
+removeFromArray([1, 2, 3, 4], 7, "tacos")
+
+// args es un array
+// https://stackoverflow.com/questions/71484291/dealing-with-multiple-optional-arguments-in-a-javascript-function
 
 // Do not edit below this line
 module.exports = removeFromArray;
