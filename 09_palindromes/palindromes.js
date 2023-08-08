8ca024949210146212fe7df8365c973e95ba49e1
GatoBinario <43298345+GatoBinario@users.noreply.github.com>
2023-08-08 17:19:05 +0200

    Solve 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,20 @@
-const palindromes = function () {
+const palindromes = function (input) {
+    input = input.replace(/[. ,\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+    input = input.toLowerCase();
 
+    let palindrome = true;
+    for (let i = 0; i < input.length/2; i++) {
+      // console.log(`${i} ${input[i]}`);
+      // console.log(`${input.length-i-1} ${input[input.length-i-1]}`);
+      palindrome = input[i] === input[input.length-1-i];
+      if (!palindrome) {
+        return false;
+      }
+    }
+    return true;
 };
+
+palindromes('A car, a man, a maraca.')
 
 // Do not edit below this line
 module.exports = palindromes;
