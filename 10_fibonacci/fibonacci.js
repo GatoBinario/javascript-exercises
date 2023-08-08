0e58b47217f998829956b90531e5cea41a1b139e
GatoBinario <43298345+GatoBinario@users.noreply.github.com>
2023-08-08 17:25:01 +0200

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function (index) {
+    if (index < 0) {
+        return "OOPS";
+    }
+    let numbers = [0, 1]
+    for (let i = 0; i < index - 1; i++) {
+        numbers.push(numbers[numbers.length - 1] + numbers[numbers.length - 2]);
+    }
+    console.log(numbers)
+    return numbers[index];
 };
 
 // Do not edit below this line
